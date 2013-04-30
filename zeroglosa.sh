57ee197a9746679444122b720cb8c718197c3d77
Halisson Vitor <halissonvit@gmail.com>
2013-04-30 10:36:44 -0300

    Adding zeroglosa mods

---
+++ zeroglosa.sh
@@ -0,0 +1,4 @@
+. /home/halisson/dev/zeroglosa/zeroglosa/ambiente/scripts/alias/variaveisDeAmbiente.bashrc
+. /home/halisson/dev/zeroglosa/zeroglosa/ambiente/scripts/alias/aliasGlobais.bashrc
+. /home/halisson/dev/zeroglosa/zeroglosa/ambiente/scripts/alias/aliasPessoais.bashrc
+
