f6c5008e43ec41fe1510e38df3a8302870f268e4
Halisson Vitor <halissonvit@gmail.com>
2021-01-12 14:53:06 -0300

    rm unnecessary files

--- zeroglosa.sh
+++
@@ -1,4 +0,0 @@
-. /home/halisson/dev/zeroglosa/zeroglosa/ambiente/scripts/alias/variaveisDeAmbiente.bashrc
-. /home/halisson/dev/zeroglosa/zeroglosa/ambiente/scripts/alias/aliasGlobais.bashrc
-. /home/halisson/dev/zeroglosa/zeroglosa/ambiente/scripts/alias/aliasPessoais.bashrc
-
